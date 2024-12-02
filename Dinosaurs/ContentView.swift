ced34275563ce82688b58b4116c61312cab773d5
kn00pers <patplaypl@gmail.com>
2024-12-02 20:12:02 +0100

    Initial Commit

---
+++ Dinosaurs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dinosaurs
+//
+//  Created by Patryk Knopp on 02/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dinosaurs/DinosaursApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinosaursApp.swift
+//  Dinosaurs
+//
+//  Created by Patryk Knopp on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DinosaursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
