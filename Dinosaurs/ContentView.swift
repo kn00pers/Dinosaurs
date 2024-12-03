6c56167cf1a4c2fed787f8b7739a6ebc527428fb
kn00pers <patplaypl@gmail.com>
2024-12-03 12:08:25 +0100

    0.0.2

--- Dinosaurs/ContentView.swift
+++ Dinosaurs/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    let predators = Predators()
+    @State var searchText = ""
+    
+    var filteredDinos: [Dinos] {
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter { predator in
+                
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationStack{
+            List(filteredDinos) { predator in
+                NavigationLink{
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label:{
+                    HStack{
+                        // image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 3)
+                        VStack(alignment: .leading){
+                            //name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            //type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                            
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Dinosaurs")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
--- Dinosaurs/Dinos.swift
+++ Dinosaurs/Dinos.swift
@@ -6,21 +6,43 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Dinos: Decodable {
+struct Dinos: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: PredatorType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable{
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum PredatorType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .gray
+            case .sea:
+                    .cyan
+            }
+        }
     }
     
 }
